af7b91f3ea5b33d8ebcd886a3c04593771a7b4c9
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-12 07:12:53 -0400

    commit tree 533d4d278000e9f714fd8eeb87b6db4d8f484003

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+//makes a POST request to /users with a name and email
+//await submitData( name, email )
+function submitData(userName, userEmail){
+    const formatObject = {
+        //.post( '/users' )
+        method: "POST",
+        //let headers
+        headers: {
+            //Add Headers(web)
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        //let reqBody
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+   
+    };
+    //Send the POST Request(web)
+    return fetch("http://localhost:3000/users", formatObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    //(web says console.log(object) but i got error, in indexTest.js it expect(document.body.innerHTML))
+      document.body.innerHTML = object.id
+  })
+  //(web) When Things Go Wrong: Using catch()
+  .catch(function (error) {
+    alert("Unauthorized Access!");
+    document.body.innerHTML="Unauthorized Access!"
+    console.log(error.message);
+    });
+}
+submitData("Sam", "sam@sam.com");
